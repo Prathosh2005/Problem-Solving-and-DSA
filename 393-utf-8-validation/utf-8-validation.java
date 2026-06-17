b3f1ea38576d5359c579d0cdd1d5a3259ebf3578
Prathosh_TS <prathoshthangaraj@gmail.com>
2026-06-17 15:30:27 +0530

    Time: 1 ms (100.00%) | Memory: 46.6 MB (48.55%) - LeetSync

--- 393-utf-8-validation/utf-8-validation.java
+++ 393-utf-8-validation/utf-8-validation.java
@@ -1,31 +1,28 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-       
        int t=0;
        for(int i=0;i<data.length;i++)
        {
-         if(data[i]>>6==0b10)
-         {
-           
-           t-=1;
-         } 
-         else if(t==0)
-         {
-            if(data[i]>>7==0b0)
+        if(t>0)
+        { if(data[i]>>6!=0b10)
+             return false;
+
+            t-=1;
+        }
+        else 
+        {
+            if(data[i]>>7==0)
                 continue;
-            else if(data[i]>>5==0b110)
+            if(data[i]>>5==0b110)
                 t=1;
             else if(data[i]>>4==0b1110)
                 t=2;
             else if(data[i]>>3==0b11110)
-                t=3;  
+                t=3;
             else
-                return false;    
-         }
-         else
-            return false;
-       }
+                return false;
+        }
+       } 
        return t==0;
-
     }
 }
