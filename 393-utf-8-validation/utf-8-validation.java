9a531320c3d3210b32071790244e44acfcbffd9c
Prathosh_TS <prathoshthangaraj@gmail.com>
2026-06-17 14:48:04 +0530

    Time: 2 ms (42.15%) | Memory: 46.5 MB (48.55%) - LeetSync

---
+++ 393-utf-8-validation/utf-8-validation.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean validUtf8(int[] data) {
+       
+       int t=0;
+       for(int i=0;i<data.length;i++)
+       {
+         if(data[i]>>6==0b10)
+         {
+           
+           t-=1;
+         } 
+         else if(t==0)
+         {
+            if(data[i]>>7==0b0)
+                continue;
+            else if(data[i]>>5==0b110)
+                t=1;
+            else if(data[i]>>4==0b1110)
+                t=2;
+            else if(data[i]>>3==0b11110)
+                t=3;  
+            else
+                return false;    
+         }
+         else
+            return false;
+       }
+       return t==0;
+
+    }
+}
